a619fee4e86c8d22e3af8c116dc76195892f8f9e
DataDroidz <94015003+DataDroidz@users.noreply.github.com>
2021-11-11 13:04:13 +0200

    Add files via upload

---
+++ Template/app.js
@@ -0,0 +1,54 @@
+(function () {
+
+    var container = document.querySelector("#unity-container");
+    var canvas = document.querySelector("#unity-canvas");
+    var loader= document.querySelector("#loader");
+    var loaderFill= document.querySelector("#fill");
+
+    function onProgress(progress) {
+        loaderFill.style.width = progress * 100 + "%";
+    }
+
+    function onComplete(unityInstance) {
+        loader.remove();
+    }
+    function onWindowResize() {
+        var width = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+
+        var height = window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+
+        canvas.height=height;
+        canvas.width=width;
+    }
+
+    var buildUrl = "Build";
+    var loaderUrl = buildUrl + "/New_Hall.loader.js";
+    var config = {
+        dataUrl: buildUrl + "/New_Hall.data",
+        frameworkUrl: buildUrl + "/New_Hall.framework.js",
+        codeUrl: buildUrl + "/New_Hall.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "Hall",
+        productVersion: "0.1",
+    };
+
+    var script = document.createElement("script");
+    script.src = loaderUrl;
+    script.onload = () => {
+        createUnityInstance(canvas, config, onProgress)
+            .then(onComplete)
+            .catch((message) => {
+                alert(message);
+        });
+    };
+    document.body.appendChild(script);
+
+    window.addEventListener('resize', onWindowResize);
+    onWindowResize();
+
+})();
